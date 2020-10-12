ef6cf7f8fd45892d76857a969a1324737a030e45
Mathew Heard <splitice@users.noreply.github.com>
2020-10-12 13:31:50 +1100

    add retry to balena push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,25 @@
 #!/bin/sh -l
 
-cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && /balena-cli/balena login --token ${API_TOKEN} && /balena-cli/balena push ${APPLICATION_NAME}
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=3
+  local delay=1
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
+cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && /balena-cli/balena login --token ${API_TOKEN} && retry /balena-cli/balena push ${APPLICATION_NAME}
